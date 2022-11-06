89818aca296d75c7a6f45cd60c4e206781fd2e54
Isaac Sunday <aiisciced12@gmail.com>
2022-11-06 22:55:26 +0100

    push 2

--- ten_students.c
+++ ten_students.c
@@ -6,14 +6,12 @@ void read_student_name(char name[])
 };
 
 int main() {
-    int count;
-    for (count= 0; count < 10; ++count)
+    for (int count = 1; count <= 10; count++)
     {
-        printf("%d\n", count);
-        // char name[] = {};
+        char name[] = {};
         printf("Please enter student name here: ");
-        // scanf("%s", name);
-        // read_student_name(name);
+        scanf("%s", name);
+        read_student_name(name);
     };
     return 0;
 }
